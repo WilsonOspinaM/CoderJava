35332469f4938b26b1d98e69ce902151ca41a5d7
WilsonOspinaM <wilsonospina@gmail.com>
2021-10-05 21:31:24 -0500

    Desafio1

---
+++ desafio1.js
@@ -0,0 +1,10 @@
+alert("Bienvenido, vamos a hacer compras");
+let frutaIngresada = prompt ("Cual de estas frutas te gustan más: manzanas, naranjas, mandarinas");
+let verduraIngresada = prompt ("Cual de estas verduras te gustan más: tomates, puerros, aguacates");
+let proteinaIngresada = prompt ("Que proteína prefieres: pescado, carne, pollo, no consumo proteína animal");
+let fruta = frutaIngresada
+let verdura = verduraIngresada
+let proteina = proteinaIngresada
+
+console.log(frutaIngresada + " " + verduraIngresada + " " + proteinaIngresada)
+alert("Hum! a mi también me gustan las " + fruta + ", los" + " " + verdura+ ", y el " + " " + proteina + ".")
---
+++ desafio2.js
@@ -0,0 +1,10 @@
+let frutaIngresada = prompt("Ingresa tu fruta preferida");
+
+if ((frutaIngresada !="") && ((frutaIngresada == "Naranja") || (frutaIngresada == "Mandarina") || (frutaIngresada == "Manzana")))
+{
+    alert("Nos gustan las mismas frutas");
+}
+else
+{
+    alert("No nos gustan las mismas frutas");
+}
